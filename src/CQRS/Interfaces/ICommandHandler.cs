a4719009a9276aaedf4d176106c4d7581873ed22
Dmitry <lux2057@gmail.com>
2023-04-05 15:32:27 +0300

    #8 Command/Query interfaces were added

---
+++ src/CQRS/Interfaces/ICommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace CRUD.CQRS
+{
+    #region << Using >>
+
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using CRUD.DAL;
+    using MediatR;
+
+    #endregion
+
+    public interface ICommandHandler<TNotification> : INotificationHandler<TNotification> where TNotification : INotification
+    {
+        #region Properties
+
+        protected IReadWriteDispatcher Dispatcher { get; }
+
+        protected IMapper Mapper { get; }
+
+        #endregion
+
+        protected Task Execute(TNotification command, CancellationToken cancellationToken);
+
+        protected IReadWriteRepository<TEntity, TId> Repository<TEntity, TId>() where TEntity : IId<TId>, new();
+    }
+}

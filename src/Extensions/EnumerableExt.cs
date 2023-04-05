c30beffa6c21d1e73f9f57e9062dd117eadc8221
Dmitry <lux2057@gmail.com>
2023-04-05 15:13:21 +0300

    #10 project init

---
+++ src/Extensions/EnumerableExt.cs
@@ -0,0 +1,18 @@
+﻿namespace CRUD.Extensions
+{
+    #region << Using >>
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    #endregion
+
+    public static class EnumerableExt
+    {
+        public static T[] ToArrayOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null ? Array.Empty<T>() : enumerable.ToArray();
+        }
+    }
+}

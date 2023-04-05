c8322fe375066bd5007991c4ac1fe4be47fca288
Dmitry <lux2057@gmail.com>
2023-04-05 14:05:24 +0300

    #2 Repository was added

---
+++ src/DAL/Interfaces/IReadRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace CRUD.DAL
+{
+    #region << Using >>
+
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using LinqSpecs;
+
+    #endregion
+
+    public interface IReadRepository<TEntity, in TId> where TEntity : IId<TId>, new()
+    {
+        Task<TEntity> GetByIdOrDefaultAsync(TId id, CancellationToken cancellationToken = default);
+
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicateExpression = null);
+
+        IQueryable<TEntity> Get(Specification<TEntity> specification);
+    }
+}
---
+++ src/DAL/Interfaces/IReadWriteRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace CRUD.DAL
+{
+    #region << Using >>
+
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    #endregion
+
+    public interface IReadWriteRepository<TEntity, in TId> : IReadRepository<TEntity, TId> where TEntity : IId<TId>, new()
+    {
+        Task AddOrUpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        Task AddOrUpdateAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(TId id, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(IEnumerable<TId> ids, CancellationToken cancellationToken = default);
+    }
+}

2895677933e51e9180243f1abe4e10afc5b2d4cf
Dmitry <lux2057@gmail.com>
2023-04-06 09:04:23 +0300

    #15 Extensions were added

--- src/Extensions/EnumerableExt.cs
+++ src/Extensions/EnumerableExt.cs
@@ -14,5 +14,12 @@
         {
             return enumerable == null ? Array.Empty<T>() : enumerable.ToArray();
         }
+
+        public static string GetJointString(this IEnumerable<string> enumerable, string separator)
+        {
+            var enumerableArray = enumerable.ToArrayOrEmpty();
+
+            return !enumerableArray.Any() ? string.Empty : string.Join(separator, enumerableArray);
+        }
     }
 }
---
+++ src/Extensions/StringExt.cs
@@ -0,0 +1,16 @@
+﻿namespace CRUD.Extensions
+{
+    #region << Using >>
+
+    using System;
+
+    #endregion
+
+    public static class StringExt
+    {
+        public static T ToEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value);
+        }
+    }
+}

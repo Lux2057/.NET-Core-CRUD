76de98d644479b289e5596a35554c8647f067072
Dmitry <lux2057@gmail.com>
2023-04-12 09:20:35 +0300

    #51 'specification' param was made optional in the Repository call

--- src/DAL/Implementations/EfReadRepository.cs
+++ src/DAL/Implementations/EfReadRepository.cs
@@ -29,7 +29,7 @@
 
         #region Interface Implementations
 
-        public IQueryable<TEntity> Get(Specification<TEntity> specification = null)
+        public IQueryable<TEntity> Get(Specification<TEntity> specification = default)
         {
             var dbSet = this._context.Set<TEntity>();
 
@@ -38,14 +38,14 @@
                            dbSet.Where(specification).AsQueryable();
         }
 
-        public IQueryable<TEntity> GetPaginated(Specification<TEntity> specification, int? page, int? pageSize)
+        public IQueryable<TEntity> GetPaginated(Specification<TEntity> specification = default, int? page = default, int? pageSize = default)
         {
             var totalCount = Get(specification).Count();
 
             return Get(specification).GetPage(totalCount, page, pageSize);
         }
 
-        public async Task<IQueryable<TEntity>> GetPaginatedAsync(Specification<TEntity> specification, int? page, int? pageSize, CancellationToken cancellationToken = default)
+        public async Task<IQueryable<TEntity>> GetPaginatedAsync(Specification<TEntity> specification = default, int? page = default, int? pageSize = default, CancellationToken cancellationToken = default)
         {
             var totalCount = await Get(specification).CountAsync(cancellationToken);
 

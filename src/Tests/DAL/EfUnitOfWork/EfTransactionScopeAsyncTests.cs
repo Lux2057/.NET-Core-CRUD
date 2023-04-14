61086ec06d27648f93b59119eb585152f11579f6
Dmitry <lux2057@gmail.com>
2023-04-14 15:03:57 +0300

    #23 DAL tests were completed

--- src/Tests/DAL/EfUnitOfWork/EfTransactionScopeAsyncTests.cs
+++ src/Tests/DAL/EfUnitOfWork/EfTransactionScopeAsyncTests.cs
@@ -4,6 +4,7 @@
 
 using System.Data;
 using CRUD.DAL;
+using Microsoft.EntityFrameworkCore;
 
 #endregion
 
@@ -30,5 +31,47 @@ public class EfTransactionScopeAsyncTests : EfUnitOfWorkTest
         await this.unitOfWork.EndTransactionScopeAsync(transactionId2);
 
         Assert.True(this.context.Database.CurrentTransaction == null);
+    }
+
+    [Fact]
+    public async Task Should_rollback_changes_in_a_transaction()
+    {
+        await this.unitOfWork.BeginTransactionScopeAsync(IsolationLevel.ReadCommitted);
+
+        var text = Guid.NewGuid().ToString();
+
+        await this.unitOfWork.ReadWriteRepository<TestEntity>().AddAsync(new TestEntity { Text = text });
+
+        var entities = await this.unitOfWork.ReadRepository<TestEntity>().Get().ToArrayAsync();
+
+        Assert.Single(entities);
+        Assert.Equal(text, entities.Single().Text);
+
+        await this.unitOfWork.RollbackCurrentTransactionScopeAsync();
+
+        entities = await this.unitOfWork.ReadRepository<TestEntity>().Get().ToArrayAsync();
+
+        Assert.Empty(entities);
+    }
+
+    [Fact]
+    public async Task Should_ignore_rolling_back_of_a_closed_transaction()
+    {
+        var transactionId = await this.unitOfWork.BeginTransactionScopeAsync(IsolationLevel.ReadCommitted);
+
+        var text = Guid.NewGuid().ToString();
+
+        await this.unitOfWork.ReadWriteRepository<TestEntity>().AddAsync(new TestEntity { Text = text });
+
+        await this.unitOfWork.EndTransactionScopeAsync(transactionId);
+
+        Assert.Null(this.context.Database.CurrentTransaction);
+
+        await this.unitOfWork.RollbackCurrentTransactionScopeAsync();
+
+        var entities = await this.unitOfWork.ReadRepository<TestEntity>().Get().ToArrayAsync();
+
+        Assert.Single(entities);
+        Assert.Equal(text, entities.Single().Text);
     }
 }

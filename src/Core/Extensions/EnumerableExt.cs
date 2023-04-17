47eeb8016ff19ddf3c3243bcdb11a4e75e63a5fa
Dmitry <lux2057@gmail.com>
2023-04-17 15:00:42 +0300

    #23 EfTests.Core project was added

--- src/Core/Extensions/EnumerableExt.cs
+++ src/Core/Extensions/EnumerableExt.cs
@@ -1,4 +1,12 @@
-﻿namespace CRUD.Core
+﻿#region << Using >>
+
+using System.Runtime.CompilerServices;
+
+#endregion
+
+[assembly: InternalsVisibleTo("EfTests.Core")]
+
+namespace CRUD.Core
 {
     #region << Using >>
 
@@ -8,11 +16,11 @@
 
     #endregion
 
-    internal static class Enumerable
+    internal static class EnumerableExt
     {
         public static TId[] GetIds<TEntity, TId>(this IEnumerable<TEntity> entities) where TEntity : IId<TId>
         {
-            return entities.Select(r => r.Id).ToArrayOrEmpty();
+            return (entities?.Select(r => r.Id)).ToArrayOrEmpty();
         }
     }
 }
---
+++ src/Tests/EfCore/Infrastructure/TestEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace EfTests.Core;
+
+#region << Using >>
+
+using CRUD.Core;
+
+#endregion
+
+public class TestEntity : Tests.Models.TestEntity, IId<int> { }

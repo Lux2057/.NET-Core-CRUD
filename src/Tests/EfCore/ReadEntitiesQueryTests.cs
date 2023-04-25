ac29730a57eb7311268fa692d603b90a5c9f0e20
Dmitry <lux2057@gmail.com>
2023-04-25 10:45:03 +0300

    #64 ReadEntitiesQuery was adjusted

--- src/Tests/EfCore/ReadEntitiesQueryTests.cs
+++ src/Tests/EfCore/ReadEntitiesQueryTests.cs
@@ -90,4 +90,58 @@ public class ReadEntitiesQueryTests : ReadDispatcherTest
         Assert.Equal(2, dtosInDb.Length);
         Assert.True(dtosInDb.All(x => x.Text == text2));
     }
+
+    [Fact]
+    public async Task Should_return_all_entities()
+    {
+        var text = Guid.NewGuid().ToString();
+
+        await this.context.Set<TestEntity>().AddRangeAsync(new[]
+                                                           {
+                                                                   new TestEntity { Text = text },
+                                                                   new TestEntity { Text = text },
+                                                                   new TestEntity { Text = text },
+                                                                   new TestEntity { Text = text },
+                                                                   new TestEntity { Text = text },
+                                                                   new TestEntity { Text = text }
+                                                           });
+
+        await this.context.SaveChangesAsync();
+
+        var dtosInDb = await this.dispatcher.QueryAsync(new ReadEntitiesQuery<TestEntity, int, TestEntityDto>
+                                                        {
+                                                                Page = 1,
+                                                                PageSize = 2,
+                                                                DisablePaging = true
+                                                        });
+
+        Assert.Equal(6, dtosInDb.Length);
+        Assert.True(dtosInDb.All(x => x.Text == text));
+    }
+
+    [Fact]
+    public async Task Should_return_entities_by_spec()
+    {
+        var text1 = Guid.NewGuid().ToString();
+        var text2 = Guid.NewGuid().ToString();
+
+        await this.context.Set<TestEntity>().AddRangeAsync(new[]
+                                                           {
+                                                                   new TestEntity { Text = text1 },
+                                                                   new TestEntity { Text = text1 },
+                                                                   new TestEntity { Text = text2 },
+                                                                   new TestEntity { Text = text2 }
+                                                           });
+
+        await this.context.SaveChangesAsync();
+
+        var dtosInDb = await this.dispatcher.QueryAsync(new ReadEntitiesQuery<TestEntity, int, TestEntityDto>
+                                                        {
+                                                                DisablePaging = true,
+                                                                Specification = new TestEntityByTextSpec(text1)
+                                                        });
+
+        Assert.Equal(2, dtosInDb.Length);
+        Assert.True(dtosInDb.All(x => x.Text == text1));
+    }
 }

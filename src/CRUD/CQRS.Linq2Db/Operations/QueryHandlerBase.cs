ac335e054e2a1d3519ecae7779f02b3dea4f3c93
Dmitry <lux2057@gmail.com>
2023-12-01 08:49:23 +0300

    #117: CQRS.Linq2Db project was added

---
+++ src/CRUD/CQRS.Linq2Db/Operations/QueryHandlerBase.cs
@@ -0,0 +1,26 @@
+﻿namespace CRUD.CQRS.Linq2Db;
+
+#region << Using >>
+
+using CRUD.DAL.Linq2Db;
+using Microsoft.Extensions.DependencyInjection;
+
+#endregion
+
+public abstract class QueryHandlerBase<TRequest, TResponse> : CRUD.CQRS.QueryHandlerBase<TRequest, TResponse> where TRequest : QueryBase<TResponse>
+{
+    #region Properties
+
+    protected new ILinq2DbReadRepository Repository { get; }
+
+    #endregion
+
+    #region Constructors
+
+    protected QueryHandlerBase(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+        Repository = serviceProvider.GetService<ILinq2DbReadRepository>();
+    }
+
+    #endregion
+}

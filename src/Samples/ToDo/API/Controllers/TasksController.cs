0f91814b21cc864af43af230a7cb4877ac57a3d8
Dmitry <lux2057@gmail.com>
2024-02-06 10:22:57 +0300

    #125: Tasks CRUD was added

---
+++ src/Samples/ToDo/API/Controllers/TasksController.cs
@@ -0,0 +1,68 @@
+﻿namespace Samples.ToDo.API;
+
+#region << Using >>
+
+using CRUD.CQRS;
+using CRUD.WebAPI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+[Authorize,
+ Route("[controller]/[action]")]
+public class TasksController : DispatcherControllerBase
+{
+    #region Constructors
+
+    public TasksController(IDispatcher dispatcher) : base(dispatcher) { }
+
+    #endregion
+
+    [HttpGet,
+     ProducesResponseType(typeof(TaskDto[]), 200)]
+    public async Task<IActionResult> Get([FromQuery] int projectId)
+    {
+        var currentUserId = await Dispatcher.QueryAsync(new GetCurrentUserIdOrDefaultQuery());
+
+        return Ok(await Dispatcher.QueryAsync(new GetTasksQuery(currentUserId, projectId)));
+    }
+
+    [HttpPost,
+     ProducesResponseType(typeof(int), 200)]
+    public async Task<IActionResult> Create([FromBody] TaskDto.CreateRequest request)
+    {
+        var currentUserId = await Dispatcher.QueryAsync(new GetCurrentUserIdOrDefaultQuery());
+
+        var command = new CreateOrUpdateTaskCommand(id: null,
+                                                    userId: currentUserId,
+                                                    name: request.Name,
+                                                    projectId: request.ProjectId,
+                                                    description: request.Description,
+                                                    dueDate: request.DueDate,
+                                                    tagsIds: request.TagsIds);
+
+        await Dispatcher.PushAsync(command);
+
+        return Ok(command.Result);
+    }
+
+    [HttpPut,
+     ProducesResponseType(typeof(int), 200)]
+    public async Task<IActionResult> Update([FromBody] TaskDto.EditRequest request)
+    {
+        var currentUserId = await Dispatcher.QueryAsync(new GetCurrentUserIdOrDefaultQuery());
+
+        var command = new CreateOrUpdateTaskCommand(id: request.Id,
+                                                    userId: currentUserId,
+                                                    name: request.Name,
+                                                    projectId: request.ProjectId,
+                                                    description: request.Description,
+                                                    dueDate: request.DueDate,
+                                                    tagsIds: request.TagsIds);
+
+        await Dispatcher.PushAsync(command);
+
+        return Ok(command.Result);
+    }
+}
--- src/Samples/ToDo/API/Models/Dtos/TaskDto.cs
+++ src/Samples/ToDo/API/Models/Dtos/TaskDto.cs
@@ -17,4 +17,44 @@ public class TaskDto
     public TagDto[] Tags { get; set; }
 
     #endregion
+
+    #region Nested Classes
+
+    public class CreateRequest
+    {
+        #region Properties
+
+        public string Name { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public int[] TagsIds { get; set; }
+
+        #endregion
+    }
+
+    public class EditRequest
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public int[] TagsIds { get; set; }
+
+        #endregion
+    }
+
+    #endregion
 }

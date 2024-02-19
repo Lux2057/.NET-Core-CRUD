ce58a310e602abb82dbb8e05229f037b2b9a851d
Dmitry <lux2057@gmail.com>
2024-02-19 08:03:09 +0300

    #125: SendAuthenticatedResponseAsync was added

---
+++ src/Samples/ToDo/Shared/Enums/HttpMethodType.cs
@@ -0,0 +1,12 @@
+﻿namespace Samples.ToDo.Shared;
+
+public enum HttpMethodType
+{
+    GET = 1,
+
+    POST = 2,
+
+    PUT = 3,
+
+    DELETE = 4
+}
--- src/Samples/ToDo/UI/Extensions/ApiExt.cs
+++ src/Samples/ToDo/UI/Extensions/ApiExt.cs
@@ -2,15 +2,49 @@
 
 #region << Using >>
 
+using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
 using CRUD.Core;
 using Extensions;
+using Fluxor;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
 using Samples.ToDo.Shared;
+using Samples.ToDo.UI.Localization;
 
 #endregion
 
 public static class ApiExt
 {
+    public static async Task<HttpResponseMessage> SendAuthenticatedRequestAsync(this HttpClient http,
+                                                                                HttpMethodType httpMethod,
+                                                                                string uri,
+                                                                                string accessToken,
+                                                                                object content = null,
+                                                                                CancellationToken cancellationToken = default)
+    {
+        var request = httpMethod switch
+        {
+                HttpMethodType.GET => new HttpRequestMessage(HttpMethod.Get, uri),
+                HttpMethodType.POST => new HttpRequestMessage(HttpMethod.Post, uri),
+                HttpMethodType.PUT => new HttpRequestMessage(HttpMethod.Put, uri),
+                HttpMethodType.DELETE => new HttpRequestMessage(HttpMethod.Delete, uri),
+                _ => throw new NotImplementedException($"{nameof(HttpMethodType)}: {httpMethod}")
+        };
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        if (httpMethod != HttpMethodType.GET && content != null)
+            request.Content = new StringContent(JsonConvert.SerializeObject(content, new JsonSerializerSettings
+                                                                                     {
+                                                                                             ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                                                                                     }), Encoding.UTF8, "application/json");
+
+        return await http.SendAsync(request, cancellationToken);
+    }
+
     public static string ToApiParams<T>(this IEnumerable<T> enumerable, string paramName)
     {
         var array = enumerable.ToArrayOrEmpty();
@@ -19,6 +53,25 @@ public static class ApiExt
             return string.Empty;
 
         return array.Select(r => $"{paramName}={r}").ToJoinedString("&");
+    }
+
+    public static async Task<TResponse> ToApiResponseOrDefaultAsync<TResponse>(this HttpResponseMessage response,
+                                                                               IDispatcher dispatcher,
+                                                                               IStringLocalizer<Resource> localization)
+    {
+        switch (response.StatusCode)
+        {
+            case HttpStatusCode.OK:
+                return await response.Content.ReadFromJsonAsync<TResponse>();
+
+            case HttpStatusCode.BadRequest:
+                dispatcher.Dispatch(new SetValidationStateWf.Init(await response.Content.ReadFromJsonAsync<ValidationFailureResult>()));
+                return default;
+
+            default:
+                dispatcher.Dispatch(new SetValidationStateWf.Init(new ValidationFailureResult($"{localization[Resource.Http_request_error]}: {response.StatusCode}", null)));
+                return default;
+        }
     }
 
     #region ToDoLists
